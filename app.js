f568a0422763bca198965e40a27f7790942762ef
dknific <dknific@gmail.com>
2022-10-12 00:20:25 -0700

    cleaned up console messages

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = 3000;
 
 const app = express();
 
-app.get('/people', async (req, res, next) => {
+app.get('/people', async (req, res) => {
     console.log(`Received GET request for resource: ${req.url}`);
 
     try {
@@ -13,10 +13,11 @@ app.get('/people', async (req, res, next) => {
         console.log('Request completed.');
     } catch (error) {
         console.error(error);
+        console.log('Could not complete request.');
     }
 });
 
-app.get('/planets', async (req, res, next) => {
+app.get('/planets', async (req, res) => {
     console.log(`Received GET request for resource: ${req.url}`);
 
     try {
@@ -25,6 +26,7 @@ app.get('/planets', async (req, res, next) => {
         console.log('Request completed.');
     } catch (error) {
         console.error(error);
+        console.log('Could not complete request.');
     }
 });
 
