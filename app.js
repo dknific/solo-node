ba907847669e99778fd9a0efa0bd8cbce775a346
dknific <dknific@gmail.com>
2022-10-12 00:10:24 -0700

    updated error catching

--- app.js
+++ app.js
@@ -5,24 +5,27 @@ const port = 3000;
 const app = express();
 
 app.get('/people', async (req, res, next) => {
-    console.log('Received GET request for resource: /people');
-    const data = await methods.getAllPeople(req.query.sortBy); 
-    res.json(data);
+    console.log(`Received GET request for resource: ${req.url}`);
 
-    if (res.statusMessage === 'OK') {
+    try {
+        const data = await methods.getAllPeople(req.query.sortBy); 
+        res.json(data);
         console.log('Request completed.');
-    } else { console.log('Could not complete request.'); }
-    
+    } catch (error) {
+        console.error(error);
+    }
 });
 
 app.get('/planets', async (req, res, next) => {
-    console.log('Received GET request for resource /planets');
-    const data = await methods.getAllPlanets(); 
-    res.json(data);
+    console.log(`Received GET request for resource: ${req.url}`);
 
-    if (res.statusMessage === 'OK') {
+    try {
+        const data = await methods.getAllPlanets(); 
+        res.json(data);
         console.log('Request completed.');
-    } else { console.log('Could not complete request.'); }
+    } catch (error) {
+        console.error(error);
+    }
 });
 
-app.listen(port, () => console.log(`Hello, Alderaan!\nExpress.js app is listening on: Port ${port}`));
+app.listen(port, () => console.log(`Hello, Alderaan!\nExpress.js app is listening on: Port ${port}\n`));
